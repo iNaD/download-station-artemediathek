1b1bcd4ab3e4d024b61217b3f0d37af434b3b1a6
Daniel Gehn <inad.gamer@gmail.com>
2015-05-11 15:38:09 +0200

    Provider refactored New build script

--- artemediathek.php
+++ artemediathek.php
@@ -2,60 +2,25 @@
 
 /**
  * @author Daniel Gehn <me@theinad.com>
- * @version 0.1
+ * @version 0.2a
  * @copyright 2015 Daniel Gehn
  * @license http://opensource.org/licenses/MIT Licensed under MIT License
  */
 
-class SynoFileHostingARTEMediathek {
-    private $Url;
-    private $Username;
-    private $Password;
-    private $HostInfo;
+require_once 'provider.php';
 
-    private $LogPath = '/tmp/arte-mediathek.log';
-    private $LogEnabled = false;
+class SynoFileHostingARTEMediathek extends TheiNaDProvider {
 
-    private $language = "de";
+    protected $LogPath = '/tmp/arte-mediathek.log';
 
-    private static $languageMap = array(
+    protected $language = "de";
+
+    protected static $languageMap = array(
         'de'    => 'de',
         'fr'    => 'vof',
     );
 
-    public function __construct($Url, $Username = '', $Password = '', $HostInfo = '') {
-        $this->Url = $Url;
-        $this->Username = $Username;
-        $this->Password = $Password;
-        $this->HostInfo = $HostInfo;
-
-        $this->DebugLog("URL: $Url");
-    }
-
-    //This function returns download url.
     public function GetDownloadInfo() {
-        $ret = FALSE;
-
-        $this->DebugLog("GetDownloadInfo called");
-
-        $ret = $this->Download();
-
-        return $ret;
-    }
-
-    public function onDownloaded()
-    {
-    }
-
-    public function Verify($ClearCookie = '')
-    {
-        $this->DebugLog("Verifying User");
-
-        return USER_IS_PREMIUM;
-    }
-
-    //This function gets the download url
-    private function Download() {
         $this->DebugLog("Determining language by url $this->Url");
 
         preg_match('#http:\/\/(?:www\.)?arte.tv\/guide\/([a-zA-Z]+)#si', $this->Url, $match);
@@ -69,41 +34,21 @@ class SynoFileHostingARTEMediathek {
 
         $this->DebugLog("Getting download url");
 
-        $curl = curl_init();
+        $rawXML = $this->curlRequest($this->Url);
 
-        curl_setopt($curl, CURLOPT_URL, $this->Url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-
-        $rawXML = curl_exec($curl);
-
-        if(!$rawXML)
+        if($rawXML === null)
         {
-            $this->DebugLog("Failed to retrieve Website. Error Info: " . curl_error($curl));
             return false;
         }
 
-        curl_close($curl);
-
         if(preg_match('#arte_vp_url=["|\'](.*?)["|\']#si', $rawXML, $match) === 1)
         {
-            $curl = curl_init();
+            $RawJSON = $this->curlRequest($match[1]);
 
-            curl_setopt($curl, CURLOPT_URL, $match[1]);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-
-            $RawJSON = curl_exec($curl);
-
-            if(!$RawJSON)
+            if($RawJSON === null)
             {
-                $this->DebugLog("Failed to retrieve JSON. Error Info: " . curl_error($curl));
                 return false;
             }
-
-            curl_close($curl);
 
             $data = json_decode($RawJSON);
 
@@ -116,17 +61,44 @@ class SynoFileHostingARTEMediathek {
             {
                 if($source->mediaType == "mp4" && mb_strtolower($source->versionShortLibelle) == $this->language && $source->bitrate > $bestSource['bitrate'])
                 {
-
                     $bestSource['bitrate'] = $source->bitrate;
                     $bestSource['url'] = $source->url;
-
                 }
             }
 
             if($bestSource['url'] !== '')
             {
+                $filename = '';
+                $url = trim($bestSource['url']);
+                $pathinfo = pathinfo($url);
+
+                $this->DebugLog("Title: " . $data->videoJsonPlayer->VTI . ' Subtitle: ' . $data->videoJsonPlayer->VSU);
+
+                if(!empty($data->videoJsonPlayer->VTI))
+                {
+                    $filename .= $data->videoJsonPlayer->VTI;
+                }
+
+                if(!empty($data->videoJsonPlayer->VSU))
+                {
+                    $filename .= ' - ' . $data->videoJsonPlayer->VSU;
+                }
+
+
+                if(empty($filename))
+                {
+                    $filename = $pathinfo['basename'];
+                }
+                else
+                {
+                    $filename .= '.' . $pathinfo['extension'];
+                }
+
+                $this->DebugLog("Naming file: " . $filename);
+
                 $DownloadInfo = array();
-                $DownloadInfo[DOWNLOAD_URL] = trim($bestSource['url']);
+                $DownloadInfo[DOWNLOAD_URL] = $url;
+                $DownloadInfo[DOWNLOAD_FILENAME] = $this->safeFilename($filename);
 
                 return $DownloadInfo;
             }
@@ -142,12 +114,5 @@ class SynoFileHostingARTEMediathek {
         return FALSE;
     }
 
-    private function DebugLog($message)
-    {
-        if($this->LogEnabled === true)
-        {
-            file_put_contents($this->LogPath, $message . "\n", FILE_APPEND);
-        }
-    }
 }
 ?>
--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
 :: Delete old data
 del artemediathek.host
+
+:: get recent version of the provider base class
+copy /Y ..\provider-boilerplate\src\provider.php provider.php
+
 :: create the .tar.gz
-7z a -ttar -so artemediathek INFO artemediathek.php | 7z a -si -tgzip artemediathek.host
+7z a -ttar -so artemediathek INFO artemediathek.php provider.php | 7z a -si -tgzip artemediathek.host
+
+del provider.php
